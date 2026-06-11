38487d32fd67ba15663cdd64ae58edfbf1c2a0bf
Sahil Salunke <sahilsalunke42@gmail.com>
2026-06-11 04:02:22 +0000

    Add KafkaListener and TransactionListener components for processing transactions

---
+++ src/main/java/com/jpmc/midascore/component/TransactionListener.java
@@ -0,0 +1,23 @@
+package com.jpmc.midascore.component;
+
+import com.jpmc.midascore.foundation.Transaction;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TransactionListener {
+
+    @KafkaListener(topics = "${general.kafka-topic}")
+    public void listen(String transactionLine) {
+        Transaction transaction = deserialize(transactionLine);
+        transaction.toString();
+    }
+
+    private Transaction deserialize(String transactionLine) {
+        String[] parts = transactionLine.split(",");
+        long senderId = Long.parseLong(parts[0].trim());
+        long recipientId = Long.parseLong(parts[1].trim());
+        float amount = Float.parseFloat(parts[2].trim());
+        return new Transaction(senderId, recipientId, amount);
+    }
+}
